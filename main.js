08009cd40c18e3f6e4f343e2d276caffa9d4f28e
cptknvckles <46335699+cptknvckles@users.noreply.github.com>
2022-06-22 14:21:49 -0500

    uploaded files

---
+++ main.js
@@ -0,0 +1,53 @@
+const countdownDisplay = document.getElementById("countdown-display")
+let rocket = document.getElementById("rocket")
+let boom = document.getElementById('boom')
+const launchEl = document.getElementById('launch')
+let timeRemaining = 5 
+let fail = Math.random() * 100
+
+function launchIt(){
+    launchEl.addEventListener('click', ()=>{
+        if(fail < 60){
+            let countDown = setInterval(()=>{
+                timeRemaining--
+                launchEl.innerHTML = `00:${timeRemaining}`
+                if(timeRemaining == 0){
+                    // launchEl.innerHTML = 'Blast Off!'
+                    clearInterval(countDown)
+                    rocket.classList.add('rocketLaunch')
+                    countdownDisplay.classList.add('countdown-display')
+                    countdownDisplay.style.opacity = 1
+                    countdownDisplay.innerHTML = ' 🎉 Blast Off! 🎉'
+                    launchEl.innerHTML = 'Launch Again?'
+                    launchEl.addEventListener('click', () =>{
+                        window.location.reload()
+                    })
+                }
+               
+                
+            },1000)
+            
+        }
+        else{
+            boom.classList.add('boom')
+            rocket.style.opacity = 0
+            boom.style.opacity = 1
+            countdownDisplay.style.opacity = 1
+            countdownDisplay.innerHTML = 'Launch Failed'
+            launchEl.innerHTML = 'Try Again!'
+            launchEl.addEventListener('click', () =>{
+                window.location.reload()
+            })
+            
+        }
+        
+    })
+}
+
+launchIt()
+
+
+
+
+
+
